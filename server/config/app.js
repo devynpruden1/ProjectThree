9a13b03105872d0f335a4846e9d0057a6becc7f9
devynpruden1 <114013771+devynpruden1@users.noreply.github.com>
2024-11-24 18:31:42 -0500

    Fixing img render

--- server/config/app.js
+++ server/config/app.js
@@ -31,8 +31,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
-app.use(express.static(path.join(__dirname, '../../node_modules')));
+app.use(express.static(path.join(__dirname, '../public'))); 
+app.use(express.static(path.join(__dirname, '../node_modules'))); 
+
 
 // Route definitions
 app.use('/', indexRouter);
